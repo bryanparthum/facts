2f464eba5f3056967c616d551cd1a4c551e930cf
Alexander Reedy <ar2208@amarel2.amarel.rutgers.edu>
2024-03-05 16:36:31 -0500

    initial commit for preprocess and utility libraries

---
+++ factslib/prelib.py
@@ -0,0 +1,84 @@
+import argparse
+import os
+import sys
+import pickle as p
+import numpy as np
+import scipy.io
+
+class PreProcess:
+
+    def __init__(self) -> None:
+        pass
+
+    def ExtractSamples(self,mat, this_corefile, targyears, baseyear):
+	    # Get the years available from the matlab core file
+        mat_years = np.squeeze(mat[this_corefile][0,0][27])
+	
+        # Determine which matlab year indices match our target years
+        mat_years_idx = np.isin(mat_years, targyears)
+	
+	    # Get the samples from the matlab core file
+        samps = mat[this_corefile][0,0][21]
+	
+	    # Extract the samples for each ice sheet
+        eais_samps = samps[:,20,:]
+        wais_samps = samps[:,19,:]
+        gis_samps = samps[:,18,:]
+	
+	    # Get the values for the baseyear of interest
+        eais_refs = np.apply_along_axis(self.FindRefVals, axis=1, arr=eais_samps, years=mat_years, baseyear=baseyear)
+        wais_refs = np.apply_along_axis(self.FindRefVals, axis=1, arr=wais_samps, years=mat_years, baseyear=baseyear)
+        gis_refs = np.apply_along_axis(self.FindRefVals, axis=1, arr=gis_samps, years=mat_years, baseyear=baseyear)
+	
+	    # Center the projections to the reference period
+        eais_samps -= eais_refs[:,np.newaxis]
+        wais_samps -= wais_refs[:,np.newaxis]
+        gis_samps -= gis_refs[:,np.newaxis]
+	
+	    # Subset for the target years
+        eais_samps = eais_samps[:,mat_years_idx]
+        wais_samps = wais_samps[:,mat_years_idx]
+        gis_samps = gis_samps[:,mat_years_idx]
+
+        return wais_samps, eais_samps, gis_samps
+    
+    def OutputDataAll(self,pipeline_id, eais_sampsH, wais_sampsH, gis_sampsH,  eais_sampsL, wais_sampsL, gis_sampsL, scenario, targyears, baseyear):
+
+	    # Sum up the components to get total AIS samples
+        ais_sampsH = eais_sampsH + wais_sampsH
+        ais_sampsL = eais_sampsL + wais_sampsL
+
+	    # Populate the output dictionary
+        outdata = {'eais_sampsH': eais_sampsH, 
+                   'wais_sampsH': wais_sampsH, 
+                   'ais_sampsH': ais_sampsH,
+                   'gis_sampsH': gis_sampsH, 
+                   'eais_sampsL': eais_sampsL, 
+                   'wais_sampsL': wais_sampsL,
+                   'ais_sampsL': ais_sampsL, 
+                   'gis_sampsL': gis_sampsL, 
+                   'targyears': targyears, 
+                   'baseyear': baseyear,
+                   'scenario': scenario
+                   }
+	
+	    # Define the data directory
+        outdir = os.path.dirname(__file__)
+
+	    # Write the rates data to a pickle file
+        outfile = open(os.path.join(outdir, "{}_data.pkl".format(pipeline_id)), 'wb')
+        p.dump(outdata, outfile)
+        outfile.close()
+
+    def FindRefVals(self,timeseries, years, baseyear):
+	
+	    # Append a zero to the beginning of the timeseries at year 2000
+        timeseries = np.append(np.array([0.0]), timeseries)
+        years = np.append(np.array([2000]), years)
+	
+	    # Interpolate to the appropriate base year
+        ref_val = np.interp(baseyear, years, timeseries, left=0.0)
+	
+	    # Return the value
+        return(ref_val)
+
---
+++ factslib/utillib.py
@@ -0,0 +1,75 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import netCDF4
+
+
+class FactsUtils:
+    def __init__(self) -> None:
+        ssp119_ref = {'bamber19_ais': [-0.01, 0.10, 0.27],
+                      'bamber19_gris': [0.07, 0.13, 0.32],
+                      }
+        
+        ssp126_ref = {'bamber19_ais': [-0.01, 0.11, 0.31],
+                      'bamber19_gris': [0.07, 0.14, 0.35]
+                      }
+        
+        ssp245_ref = {'bamber19_ais': [-0.00, 0.14, 0.43],
+                      'bamber19_gris': [0.08, 0.16, 0.44]
+                      }
+        
+        ssp370_ref = {'bamber19_ais': [0.01, 0.17, 0.5],
+                      'bamber19_gris': [0.09, 0.19, 0.53]
+                      }
+        
+        ssp585_ref = {'bamber19_ais': [0.02, 0.19, 0.55],
+                      'bamber19_gris': [0.09, 0.21, 0.35]
+                      }
+
+        self.reference_dicts = {'ssp119': ssp119_ref,
+                                'ssp126':ssp126_ref,
+                                'ssp245':ssp245_ref,
+                                'ssp370':ssp370_ref,
+                                'ssp585':ssp585_ref}
+        
+
+    # This function should be placed in the projection stage of the module.
+    # Note this automatically converts the runs projection data to m
+    def verify_module_output(self, scenario, module_set, region, filename, year=2100):
+
+        quantiles = [.17, .50, .83]
+
+        # Opens the projection.nc file for the run data
+        run_data = xr.open_dataset(filename)
+
+        # loads the reference dictionary for this SSP
+        reference_dict = self.reference_dicts[f'{scenario}'][f'{module_set}_{region}']
+
+        # Create the pandas dataframe to output quantile information
+        quantile_data = {'REF': [], 'RUN': [], '%DIFF': []}
+        quantile_data = pd.DataFrame(quantile_data)
+
+        # Loops through the quantiles and adds them to the data frame
+        for i in range(len(quantiles)):
+            ref_quant = reference_dict[i]
+            run_quant = self.get_quantile(data=run_data, year=year, quantile=quantiles[i])
+            diff = np.round(((run_quant - ref_quant) / run_quant) * 100,2)
+            quantile_data.loc[i, 'REF'] = ref_quant
+            quantile_data.loc[i, 'RUN'] = run_quant
+            quantile_data.loc[i, '%DIFF'] = diff
+
+        # Renames the indicies of the dataframe
+        quantile_data.index = ['Q17', 'Q50', 'Q83']
+
+        quantile_data.to_csv('quantile_verification.txt')
+        return(quantile_data)
+
+    def get_quantile(self, data, year, quantile, conversion=1000, round_quant=True):
+        quantile = float(
+            data.sea_level_change.sel(years=year, drop=True).quantile([quantile], dim='samples') / conversion)
+        if round_quant:
+            quantile = np.round(quantile, 2)
+        return quantile
+        
